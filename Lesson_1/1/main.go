d70ab4793cc31e066468e54bcdf00dff2a50f1e7
Vladyslav Radchuk <cepishmohnoril@gmail.com>
2021-06-29 00:48:34 +0300

    first hands on practice, structs, funcs and interfaces

--- Lesson_1/1/main.go
+++ Lesson_1/1/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+)
 
+type square struct {
+	side float64
+}
+
+type circle struct {
+	radius float64
+}
+
+type shape interface {
+	area() float64
+}
+
+func (s square) area() float64 {
+	return s.side * s.side
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func printArea(s shape) {
+	fmt.Println(s.area())
+}
+
+func main() {
+	sqr := square{side: 5}
+	cir := circle{radius: 10}
+
+	printArea(sqr)
+	printArea(cir)
 }
