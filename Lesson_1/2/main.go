771ad357aa6b110fa7a81692c87de05d74a84a5c
Vladyslav Radchuk <cepishmohnoril@gmail.com>
2021-06-29 08:23:43 +0300

    second hands on practice

--- Lesson_1/2/main.go
+++ Lesson_1/2/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import "fmt"
 
+type person struct {
+	fName string
+	lName string
+}
+
+type secretAgent struct {
+	person
+	ltk bool
+}
+
+func (p person) pSpeak() {
+	fmt.Println(p.fName, " person speaks")
+}
+
+func (sa secretAgent) saSpeak() {
+	fmt.Println(sa.fName, " has ltk:", sa.ltk)
+}
+
+func main() {
+	per := person{"Foo", "Bar"}
+	sa := secretAgent{person{"Jame", "Son"}, true}
+	fmt.Println(per.fName)
+	per.pSpeak()
+	fmt.Println(sa.ltk)
+	sa.saSpeak()
+	sa.person.pSpeak()
 }
