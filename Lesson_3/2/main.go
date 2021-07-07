3ae8e0dc4bcddefdc5a4cd30ac6c0b27527e8823
Vladyslav Radchuk <cepishmohnoril@gmail.com>
2021-07-07 16:34:27 +0300

    second server with simple routing

--- Lesson_3/2/main.go
+++ Lesson_3/2/main.go
@@ -1,5 +1,107 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
 
+func main() {
+	listener, err := net.Listen("tcp", ":8080")
+
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println(err.Error())
+			continue
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	defer conn.Close()
+
+	i := 0
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+
+		if i == 0 {
+			if strings.Fields(ln)[0] == "GET" {
+				processGet(conn, strings.Fields(ln)[1])
+				break
+			}
+		}
+
+		if ln == "" {
+			break
+		}
+
+		i++
+	}
+
+}
+
+func processGet(conn net.Conn, route string) {
+
+	if route == "/some" {
+		somePage(conn)
+	}
+
+	if route == "/other" {
+		otherPage(conn)
+	}
+
+}
+
+func somePage(conn net.Conn) {
+
+	body := `
+		<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<title></title>
+			</head>
+			<body>
+				<strong>This is some page.</strong>
+			</body>
+		</html>
+	`
+
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, body)
+}
+
+func otherPage(conn net.Conn) {
+
+	body := `
+		<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<title></title>
+			</head>
+			<body>
+				<strong>This is other page.</strong>
+			</body>
+		</html>
+	`
+
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, body)
 }
