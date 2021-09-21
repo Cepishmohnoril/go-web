d763d0fcb8ef66e1d9653510466ad5c05c0f60df
Vladyslav Radchuk <cepishmohnoril@gmail.com>
2021-09-21 22:46:03 +0300

    first mongo handson

---
+++ Lesson_15/1/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"go-web/Lesson_15/1/controllers"
+	"go-web/Lesson_15/1/models"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	r := httprouter.New()
+	// Get a UserController instance
+	uc := controllers.NewUserController(getSession())
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+	http.ListenAndServe("localhost:8080", r)
+}
+
+func getSession() map[string]models.User {
+	return make(map[string]models.User)
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module go-web
 go 1.16
 
 require (
-	github.com/satori/go.uuid v1.2.0 // indirect
-	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/satori/go.uuid v1.2.0
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
 )
